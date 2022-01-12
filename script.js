c971a3fbe0b3b65f6937416afcd3b1f0adfe7027
PplUmangKamdar <umang.kamdar@playpowerlabs.com>
2022-01-12 23:33:20 +0530

    Added database for todos app

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-getToDo();
+var allTodos;
+async function getapi() {
+  // Storing response
+  const response = await fetch("http://localhost:5000/todos");
 
-//add task
+  // Storing data in form of JSON
+  var data = await response.json();
+
+  getToDo(data);
+
+  return data;
+}
+
+allTodos = getapi();
+
 const add = document.getElementById("add-btn");
 const txtInput = document.querySelector(".txt-input");
-add.addEventListener("click", function () {
+add.addEventListener("click", async function () {
   const item = txtInput.value.trim();
   if (item) {
     txtInput.value = "";
 
-    const todos = !localStorage.getItem("todos")
-      ? []
-      : JSON.parse(localStorage.getItem("todos"));
-
-    var currentTodo = {};
-    // if (todos.length === 0) {
-    //   currentTodo = {
-    //     id: 0,
-    //     item,
-    //     isCompleted: false,
-    //   };
-    // } else {
-    currentTodo = {
+    var currentTodo = {
       id: "id-" + new Date().getTime(), // unqiue id
-      item,
+      item: item,
       isCompleted: false,
     };
-    // }
 
     getToDo([currentTodo]);
-    todos.push(currentTodo);
-    localStorage.setItem("todos", JSON.stringify(todos));
+
+    await fetch("http://localhost:5000/todos", {
+      method: "POST",
+      body: JSON.stringify(currentTodo),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
   }
   txtInput.focus();
 });
@@ -40,19 +46,7 @@ txtInput.addEventListener("keydown", function (e) {
   }
 });
 
-//checkbox
-function stateTodo(id, completed) {
-  console.log("id: ", id);
-  const todos = JSON.parse(localStorage.getItem("todos"));
-
-  var index = todos.findIndex(std => std.id === id); // findIndex() is a function o find the Index from json object
-
-  todos[index].isCompleted = completed;
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-//Show Todos
-function getToDo(todos = JSON.parse(localStorage.getItem("todos"))) {
+async function getToDo(todos = allTodos) {
   if (!todos) {
     return null;
   }
@@ -61,18 +55,21 @@ function getToDo(todos = JSON.parse(localStorage.getItem("todos"))) {
     console.log("todo: ", todo);
     const card = document.createElement("li");
     const divElement = document.createElement("div");
+    const item = document.createElement("p");
     const checkBoxInput = document.createElement("input");
 
-    const item = document.createElement("p");
+    const delButton = document.createElement("button");
 
     card.classList.add("card");
     divElement.classList.add("cb-container");
     checkBoxInput.classList.add("cb-input");
     item.classList.add("item");
+    delButton.classList.add("del");
 
     checkBoxInput.setAttribute("type", "checkbox");
     card.setAttribute("id", todo.id);
 
+    delButton.textContent = "Delete";
     item.textContent = todo.item;
 
     if (todo.isCompleted) {
@@ -81,13 +78,13 @@ function getToDo(todos = JSON.parse(localStorage.getItem("todos"))) {
     }
 
     // Add event listener for checkbox
-    checkBoxInput.addEventListener("click", function () {
+    checkBoxInput.addEventListener("click", async function () {
       const selectedCard = this.parentElement.parentElement;
       const checked = this.checked;
       console.log("card: ", selectedCard);
       const id = selectedCard.getAttribute("id");
 
-      stateTodo(id, checked);
+      await stateTodo(id, checked);
 
       checked
         ? selectedCard.classList.add("checked")
@@ -95,6 +92,7 @@ function getToDo(todos = JSON.parse(localStorage.getItem("todos"))) {
 
       divElement.appendChild(checkBoxInput);
       divElement.appendChild(item);
+      divElement.appendChild(delButton);
 
       if (card.classList.contains("checked")) {
         card.appendChild(divElement);
@@ -105,14 +103,53 @@ function getToDo(todos = JSON.parse(localStorage.getItem("todos"))) {
       }
     });
 
+    delButton.addEventListener("click", async function () {
+      const selectedCard = this.parentElement.parentElement;
+      const checked = this.checked;
+      console.log("card: ", selectedCard);
+      const id = selectedCard.getAttribute("id");
+
+      await deleteTodo(id);
+
+      if (card.classList.contains("checked")) {
+        document.querySelector(".comp").removeChild(card);
+      } else {
+        document.querySelector(".not-comp").removeChild(card);
+      }
+    });
+
     divElement.appendChild(checkBoxInput);
     divElement.appendChild(item);
+    divElement.appendChild(delButton);
+
     if (card.classList.contains("checked")) {
       card.appendChild(divElement);
+
       document.querySelector(".comp").appendChild(card);
     } else {
       card.appendChild(divElement);
+
       document.querySelector(".not-comp").appendChild(card);
     }
+  });
+}
+
+//checkbox
+async function stateTodo(id, completed) {
+  const state = {
+    isCompleted: completed,
+  };
+
+  await fetch("http://localhost:5000/todos/" + id, {
+    method: "PUT",
+    body: JSON.stringify(state),
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+  });
+}
+
+async function deleteTodo(id) {
+  await fetch("http://localhost:5000/todos/" + id, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
   });
 }
